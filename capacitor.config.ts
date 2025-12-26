d1b34dc426668e42ee2d23d02ed0ca9e4ff63b90
surya <suryamanojphy31@gmail.com>
2025-12-26 10:27:22 +0530

    chore: update dependencies for new packages

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,21 @@ const config: CapacitorConfig = {
   appId: 'com.swecha.corpus',
   appName: 'corpus-frontend',
   webDir: 'dist',
+  backgroundColor: '#00000000',
+  plugins: {
+    Camera: {
+      permissions: ['camera', 'microphone'],
+    },
+    CapacitorVideoRecorder: {
+      permissions: ['camera', 'microphone', 'storage'],
+    },
+    VoiceRecorder: {
+      permissions: ['microphone'],
+    },
+    Filesystem: {
+      permissions: ['storage'],
+    },
+  },
 };
 
 export default config;
