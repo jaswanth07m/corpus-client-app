2e192fed52146d3543c9c5f13edbff4d50938a78
srilathabandari <srilathabandari18@gmail.com>
2026-03-28 11:51:38 +0530

    fix: pipeline failures

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,1 +1,12 @@
 import '@testing-library/jest-dom';
+
+// Mock ResizeObserver which is used by cmdk but not available in jsdom
+global.ResizeObserver = class ResizeObserver {
+  constructor(callback: ResizeObserverCallback) {}
+  disconnect() {}
+  observe(target: Element, options?: ResizeObserverOptions) {}
+  unobserve(target: Element) {}
+};
+
+// Mock scrollIntoView which is used by cmdk but not available in jsdom
+Element.prototype.scrollIntoView = vi.fn();
